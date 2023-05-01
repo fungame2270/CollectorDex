2c5aa5879285deb808dcf249d19ae535bf9d3604
Barb02 <98841800+Barb02@users.noreply.github.com>
2023-05-01 14:28:03 +0100

    all collections page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import CollectionCard from './components/CollectionCard';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-4xl font-bold">All Collections</h1>
-      <div className="grid grid-cols-3">
+    <div className="App mx-5 text-gray-950">
+      <nav className="flex space-x-4 py-3">
+        <ul>Home</ul>
+        <ul>All Collections</ul>
+      </nav>
+      <div className="my-12">
+        <h1 className="text-4xl font-bold text-start">All Collections</h1>
+      </div>
+      <div className="grid grid-cols-3 py-3 gap-12">
+        < CollectionCard />
+        < CollectionCard />
         < CollectionCard />
         < CollectionCard />
         < CollectionCard />
--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,13 +1,10 @@
 function CollectionCard() {
     return (
-      <div className="card w-96 h-72 bg-base-100 shadow-xl">
+      <div className="card w-96 h-72 shadow-xl">
         <figure><img src="https://i.imgur.com/00zlJWq.jpeg" alt="Shoes" /></figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
+          <h2 className="card-title font-bold">Shoes!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
         </div>
       </div>
     );
