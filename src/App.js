0ac238c4a9633ec18d687d63e99fab9cbec58175
Barb02 <98841800+Barb02@users.noreply.github.com>
2023-05-02 14:17:03 +0100

    items centered

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="my-12">
         <h1 className="text-4xl font-bold text-start">All Collections</h1>
       </div>
-      <div className="grid grid-cols-3 py-3 gap-12">
+      <div className="grid grid-cols-3 py-3 gap-y-12 justify-items-center">
         < CollectionCard />
         < CollectionCard />
         < CollectionCard />
