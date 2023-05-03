48ae52fc19d3477c2bbb95faba240bca98f54cd5
Barb02 <98841800+Barb02@users.noreply.github.com>
2023-05-03 16:07:13 +0100

    buttons

--- src/routes/AllCollections.js
+++ src/routes/AllCollections.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import CollectionCard from '../components/CollectionCard';
+import { ReactComponent as Filter } from "../images/filter.svg";
 
 function AllCollections() {
   return (
@@ -8,8 +9,13 @@ function AllCollections() {
         <ul><Link to="/">Home</Link></ul>
         <ul><Link to="/AllCollections">All Collections</Link></ul>
       </nav>
-      <div className="my-12">
+      <div className="grid grid-cols-2 my-12">
         <h1 className="text-4xl font-bold text-start">All Collections</h1>
+        <div className="flex flex-row-reverse gap-4 justify-items-end">
+           <button className="btn btn-primary w-20 drop-shadow">New +</button>
+           <button className="btn btn-primary w-28 drop-shadow"> <p className="mr-2">Filter</p> <Filter></Filter> </button>
+           <form className=""></form>
+        </div>
       </div>
       <div className="grid grid-cols-3 py-3 gap-12 justify-items-center">
         < CollectionCard />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,23 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+    {
+      mytheme: {
+        "primary": "#ffffff",
+        "secondary": "#D926AA",
+        "accent": "#1FB2A5",
+        "neutral": "#191D24",
+        "base-100": "#2A303C",
+        "info": "#3ABFF8",
+        "success": "#36D399",
+        "warning": "#FBBD23",
+        "error": "#F87272",
+      },
+    },
+    ],
+  }
 }
+
 
